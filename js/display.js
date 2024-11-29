f1b7eabcb0b5425215da99c8e8f74264a8a1a20a
Maria Kjellholm <maria.kjellholm@gmail.com>
2024-11-29 14:06:24 +0100

    klar tror jag

---
+++ js/display.js
@@ -0,0 +1,42 @@
+//modul för att display
+import { elements, planetColors } from "./elem.js";
+
+export const displayInfo = (bodyInfo) => {
+    elements.startSite.style.display = "none";
+    elements.planetsSite.style.display = "flex";
+    
+    elements.planetName.innerText = bodyInfo.name.toUpperCase();
+    elements.planetNameLatin.innerText = bodyInfo.latinName.toUpperCase();
+    elements.planetText.innerText = bodyInfo.desc;
+    elements.dataCircumference.innerText = `${bodyInfo.circumference.toLocaleString()} KM`;
+    elements.dataMaxTemp.innerText = `${bodyInfo.temp.day} °C`;
+    elements.dataMinTemp.innerText = `${bodyInfo.temp.night} °C`;
+    elements.dataKmSun.innerText = `${bodyInfo.distance.toLocaleString()} KM`;
+  
+    if (bodyInfo.moons.length === 0) {
+      elements.moonList.innerText = 'Denna himlakropp har inga månar'
+    } else {
+      bodyInfo.moons.forEach((element) => {    
+        let moon = document.createElement("p");
+        moon.className = "data";
+        elements.moonList.appendChild(moon);
+        moon.innerText = element 
+      });
+    }
+    
+    elements.exitBtn.addEventListener("click", () => {
+      elements.moonList.innerText = "";
+      elements.startSite.style.display = "flex";
+      elements.planetsSite.style.display = "none";
+    });
+  };
+  
+export const changePlanetColor = (bodyInfo) => {
+    for (let index = 0; index < planetColors.length; index++) {
+        if (index == bodyInfo.id) {
+            elements.bigPlanetOne.style.background = planetColors[index].fig1 
+            elements.bigPlanetTwo.style.background = `${planetColors[index].fig2}`
+            elements.bigPlanetThree.style.background = `${planetColors[index].fig3}`
+        } 
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //main-modul som hämtar info från de andra modulerna när man klickar på en planet
-
-import { elements, bodiesElement, planetColors } from "./elem.js";
+import { bodiesElement } from "./elem.js";
 import { getKey, getPlanets} from "./api.js";
+import { displayInfo } from "./display.js"
 let apiKey = null;
 
 const initEventListener = async () => {
@@ -25,47 +25,5 @@ const initEventListener = async () => {
     });
   }
 };
-
-const displayInfo = (bodyInfo) => {
-  elements.startSite.style.display = "none";
-  elements.planetsSite.style.display = "flex";
-  
-  elements.planetName.innerText = bodyInfo.name.toUpperCase();
-  elements.planetNameLatin.innerText = bodyInfo.latinName.toUpperCase();
-  elements.planetText.innerText = bodyInfo.desc;
-  elements.dataCircumference.innerText = `${bodyInfo.circumference} KM`;
-  elements.dataMaxTemp.innerText = `${bodyInfo.temp.day} °C`;
-  elements.dataMinTemp.innerText = `${bodyInfo.temp.night} °C`;
-  elements.dataKmSun.innerText = `${bodyInfo.distance} KM`;
-
-  if (bodyInfo.moons.length === 0) {
-    elements.moonList.innerText = 'Denna himlakropp har inga månar'
-  } else {
-    bodyInfo.moons.forEach((element) => {    
-      let moon = document.createElement("p");
-      moon.className = "data";
-      elements.moonList.appendChild(moon);
-      moon.innerText = element 
-    });
-    
-  }
-  
-  elements.exitBtn.addEventListener("click", () => {
-    elements.moonList.innerText = "";
-    
-    elements.startSite.style.display = "flex";
-    elements.planetsSite.style.display = "none";
-  });
-};
-
-const changePlanetColor = (bodyInfo) => {
-  for (let index = 0; index < planetColors.length; index++) {
-    if (index == bodyInfo.id) {
-      elements.bigPlanetOne.style.background = planetColors[index].fig1 
-      elements.bigPlanetTwo.style.background = `${planetColors[index].fig2}`
-      elements.bigPlanetThree.style.background = `${planetColors[index].fig3}`
-    } 
-  }
-}
 
 initEventListener();
